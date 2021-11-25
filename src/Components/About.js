698f62fce102ba87cdb625b16fbeb62689365721
amcmullen9291 <amcmullen9291@gmail.com>
2021-11-25 16:49:50 -0500

    added sweetalert2 dependency

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,13 +1,26 @@
 import React from 'react'
 
 export default function About() {
+
+    function centerHeader(){
+        let signs = Document.getElementByClass("header");
+        signs.setAttribute("align", "center");
+        
+
+    }
     return (
         <>
       <div>
-          <center><h1>React At-A-Glance</h1></center>
-          <center>testing the router from the React Intro page</center>
+          <center><h1 id="welcome">React At-A-Glance</h1></center>
       </div>
-      <center><a href={"/"}><button>return home</button></a></center>
+      <header>
+          <h1 class="header">This h1 element is in the header.</h1>
+          <p class="header">(but wouldn't it look nicer in the middle?)</p><button onClick={centerHeader()}>Click to move it</button>
+      </header>
+      <footer>
+          <br/>
+      <center><a href={"/"}><button id="homeButton">return home</button></a></center>
+      </footer>
       </>
     )
 
