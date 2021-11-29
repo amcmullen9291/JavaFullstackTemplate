d63c8c51ac2b8239edd17a61dbac0e21dd9dedec
amcmullen9291 <amcmullen9291@gmail.com>
2021-11-29 03:08:11 -0500

    Action and Reducer page

--- src/Components/ActionsReducers.js
+++ src/Components/ActionsReducers.js
@@ -3,11 +3,38 @@ import React from 'react'
 export default function ActionsReducers() {
     return (
         <>
-      <div>
-          <center><h1>Actions and Reducers</h1></center>
-          <center>testing the router from the Actions and Reducers page</center>
-      </div>
-      <center><a href={"/"}><button>return home</button></a></center>
+        <div className="background">
+    <div>
+          <center><h1 id="welcome">Actions <br/>and Reducers</h1></center>
+    </div>
+    <div id="actionArea">
+    <pre>{`
+export const setUserName = (userName) => {
+    return {
+        type: ActionTypes.SET_NAME,
+        payload: userName
+    }
+};
+    `}</pre>
+<span><center>An Action is used to describe what is taking place.<br/></center></span>
+</div>
+<div id="reducerArea">
+<pre>{`
+export const NameReducer = (state=name, {type, payload}) => {    
+    switch (type) {
+        case ActionTypes.SET_NAME:
+            return { ...state, yourName: payload};
+        default:
+            return state
+    }
+};
+`}</pre>
+<span><center>A Reducer returns a single value.<br/></center></span>
+    </div>
+    <footer>
+        <center><a href={"/"}><button>return home</button></a></center>
+    </footer>
+    </div>
       </>
     )
 }
